17b19538ec827c3ec065709d08b4c928efb428ad
Enrico Marino <enrico.marino@email.com>
2017-05-28 22:24:26 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+class PromiseBack {
+
+  constructor (fn, callback) {
+    if (typeof callback !== 'function') {
+      return new Promise(fn)
+    }
+
+    return new Promise((resolve, reject) => {
+      let resolve_back = (res) => {
+        resolve(res)
+        callback(null, res)
+      }
+
+      let reject_back = (err) => {
+        reject(err)
+        callback(err, null)
+      }
+
+      fn(resolve_back, reject_back)
+    })
+  }
+
+}
+
+module.export = PromiseBack
