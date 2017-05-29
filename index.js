6b963d743be03e1b62060f329943a939fc571379
Enrico Marino <enrico.marino@email.com>
2017-05-30 01:07:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 class PromiseBack {
 
+  static reject (err, callback) {
+    callback(err)
+    return Promise.reject(err)
+  }
+
+  static resolve (res, callback) {
+    callback(null, res)
+    return Promise.resolve(res)
+  }
+
   constructor (fn, callback) {
     if (typeof callback !== 'function') {
       return new Promise(fn)
@@ -7,13 +17,13 @@ class PromiseBack {
 
     return new Promise((resolve, reject) => {
       let resolve_back = (res) => {
-        resolve(res)
         callback(null, res)
+        resolve(res)
       }
 
       let reject_back = (err) => {
-        reject(err)
         callback(err, null)
+        reject(err)
       }
 
       fn(resolve_back, reject_back)
